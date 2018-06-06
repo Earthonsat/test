f372a4511da47f25ca176316c5d9955801527c3b
Julien Mahe <julmahe@FRPARPWW1049.global.publicisgroupe.net>
2018-06-06 17:34:31 +0200

    updates

--- action-change-light.py
+++ action-change-light.py
@@ -5,6 +5,7 @@ import ConfigParser
 from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
 import io
+import logging
 from RPi import GPIO
 
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
@@ -26,26 +27,34 @@ class SnipsConfigParser(ConfigParser.SafeConfigParser):
 
     def subscribe_intent_callback(hermes, intentMessage):
         conf = read_configuration_file(CONFIG_INI)
+        logging.debug('intent subscribed')
         action_wrapper(hermes, intentMessage, conf)
 
     def action_wrapper(hermes, intentMessage, conf):
+        logging.debug('action wrapper')
+        result_sentence = "Sorry"
         if len(intentMessage.slots.state) > 0:
             state = intentMessage.slots.state.first().value
             if state == "on":
                 result_sentence = "Turning the light ON."
+                logging.debug('turn the light ON')
                 GPIO.output(14, GPIO.HIGH)
             else:
                 result_sentence = "Turning the light OFF."
+                logging.debug('turn the light OFF')
                 GPIO.output(14, GPIO.LOW)
         else:
-            result_sentence = 	"Nope"
+            result_sentence = "Nope"
+            logging.debug('Error')
         ​
         current_session_id = intentMessage.session_id
         hermes.publish_end_session(current_session_id, result_sentence)
 
     if __name__ == "__main__":
+        logging.basicConfig(filename='/home/pi/debug.log',level=logging.DEBUG)
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(14, GPIO.OUT)
         GPIO.output(14, GPIO.LOW)
+        logging.debug('READY')
         with Hermes("localhost:1883") as h:
             h.subscribe_intent("jumahe:change-light", subscribe_intent_callback).start()
